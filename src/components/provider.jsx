cd58dee8abe00a5d7734282f795f5d1937c6bc3c
MikhailAG <mikhail.gubanov.a@gmail.com>
2024-10-08 00:09:36 +0300

    provider update, form, index update

--- src/components/provider.jsx
+++ src/components/provider.jsx
@@ -1,10 +1,12 @@
-import {Routes, Route} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import {Login} from './login.jsx'
 
 export function Provider() {
     return (
+      <Router>
         <Routes>
-            <Route path="/login" element={<Login />} />
+          <Route path="/login" element={<Login/>}/>
         </Routes>
+      </Router>
     )
 }
